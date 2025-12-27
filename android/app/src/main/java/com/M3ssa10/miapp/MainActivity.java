b457b496dd22c863fe379aaa6dfc6db5aff98d5e
Scrapp Power <scrapp@power.com>
2025-12-27 20:39:14 +0100

    Register FolderTxtReaderPlugin via registerPlugin

--- android/app/src/main/java/com/M3ssa10/miapp/MainActivity.java
+++ android/app/src/main/java/com/M3ssa10/miapp/MainActivity.java
@@ -2,15 +2,11 @@ package com.M3ssa10.miapp;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.PluginManager;
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    // Registrar plugin SIN referenciar la clase (evita problema Java->Kotlin)
-    PluginManager.addPluginClass(com.M3ssa10.miapp.FolderTxtReaderPlugin.class);
+    registerPlugin(FolderTxtReaderPlugin.class);
   }
 }
-
